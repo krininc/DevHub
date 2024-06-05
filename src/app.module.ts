8c3e3712c34b2960befa096d5c4a0b5d1a0c138c
Kristijan Nincevic <kn1059@rit.edu>
2024-06-05 14:05:50 +0200

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'your-vercel-database-host',
+      port: 3306,
+      username: 'your-username',
+      password: 'your-password',
+      database: 'your-database',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
